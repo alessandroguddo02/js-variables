a378339399ea7bb7b18d6dcbf3573c7100fc4565
alessandroguddo02 <guddoalessandro6@gmail.com>
2024-10-25 17:14:54 +0200

    add part of bonus

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -22,10 +22,59 @@ console.log(cityReplaced); // Output:  "New Delhi"
 // 4. Template literal
 // Concatena due stringhe usando la sintassi `${var}`
 // e rendi cityNameConcat uguale a 'New York City'
-const cityNameConcat = `${city} ${cityName}` ;
+const cityNameConcat = `${city} ${cityName}`;
 console.log(cityNameConcat); // Output: "New York City"
 
 // 5. Tutto in maiscolo
 // Trasforma tutto in maiscuolo la stringa della variabile hello
 const upperCaseHello = hello.toUpperCase();
 console.log(upperCaseHello); // Output: "HELLO"
+
+
+
+
+///////////////////////////////////////////////////////////
+
+
+
+
+const mountain = "Monte Bianco"; // NON MODIFICARE QUESTA RIGA
+const activities = "Escursionismo, Arrampicata, Sci, Fotografia"; // NON MODIFICARE QUESTA RIGA
+const greeting = "Buona giornata"; // NON MODIFICARE QUESTA RIGA
+const elevation = "4810"; // NON MODIFICARE QUESTA RIGA
+const weather = "nebbioso"; // NON MODIFICARE QUESTA RIGA
+
+// 1. Estrai un carattere specifico
+// Estrai il carattere alla posizione 6 nella variabile mountain usando charAt()
+const sixthChar = mountain.charAt(6); // Indici partono da 0
+console.log(sixthChar);
+
+// 2. Verifica se una stringa contiene una sottostringa
+// Verifica se la variabile activities contiene la parola 'Sci' e stampa il risultato (true/false)
+const hasSki = activities.includes("Sci");
+console.log(hasSki);
+
+// 3. Converti una stringa in numero e fai un calcolo
+// Converti la variabile elevation in un numero e aggiungi 10, poi stampa il risultato
+const elevationNumber = Number(elevation) + (10);
+console.log(elevationNumber);
+
+// 4. Trasforma tutto in minuscolo
+// Trasforma tutto in minuscolo la stringa della variabile greeting
+const lowerCaseGreeting = "";
+console.log(lowerCaseGreeting);
+
+// 5. Ripeti una stringa
+// Ripeti la stringa weather 3 volte con uno spazio tra le ripetizioni e stampa il risultato
+const repeatedWeather = "";
+console.log(repeatedWeather);
+
+// 6. Estrai una sottostringa usando substr()
+// Estrai 'Bianco' dalla variabile mountain usando substr()
+const mountainSubstr = false;
+console.log(mountainSubstr);
+
+// 7. Aggiungi padding a una stringa
+// Aggiungi zeri all'inizio della variabile elevation per renderla lunga 6 caratteri
+const paddedElevation = true;
+console.log(paddedElevation);
